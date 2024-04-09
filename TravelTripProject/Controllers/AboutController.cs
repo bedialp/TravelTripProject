dd36a9f2dae03dcc4559f16febcd5319ebac1b4f
bedialp <fvzlpktk@gmail.com>
2024-04-09 20:57:38 +0300

    Add project files.

---
+++ TravelTripProject/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProject.Models.Siniflar;
+
+namespace TravelTripProject.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        Context c = new Context(); //context sinifindan c nesnesi turettik.
+        public ActionResult Index()
+        {
+            var degerler =c.Hakkimizdas.ToList(); //c nesnesi ile ulastigimiz tablolardan bir deger urettik.
+            return View(degerler); // urettigimiz degeri view`da cagirdik.
+        }
+    }
+}

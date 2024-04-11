68f1cca58b751273bf23dc9ca6f7b5de87ea7bda
bedialp <fvzlpktk@gmail.com>
2024-04-11 20:38:50 +0300

    Section up to lesson 29

--- TravelTripProject/Controllers/BlogController.cs
+++ TravelTripProject/Controllers/BlogController.cs
@@ -11,10 +11,22 @@ namespace TravelTripProject.Controllers
     {
         // GET: Blog
         Context c = new Context();
+        BlogYorum by = new BlogYorum();
+
         public ActionResult Index()
         {
-            var bloglar = c.Blogs.ToList();
-            return View(bloglar);
+            //var bloglar = c.Blogs.ToList();
+            by.Deger1 = c.Blogs.ToList();
+            by.Deger3 = c.Blogs.OrderByDescending(item3=>item3.ID).Take(3).ToList();
+            return View(by);
+        }
+        public ActionResult BlogDetay(int id)
+        {
+            //var blogbul = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger1 = c.Blogs.Where(x => x.ID == id).ToList();
+            by.Deger2 = c.Yorumlars.Where(x => x.Blogid == id).ToList();
+            by.Deger3 = c.Blogs.OrderByDescending(item3 => item3.ID).Take(3).ToList();
+            return View(by);
         }
     }
 }
--- TravelTripProject/Controllers/DefaultController.cs
+++ TravelTripProject/Controllers/DefaultController.cs
@@ -3,19 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Razor.Parser.SyntaxTree;
+using TravelTripProject.Models.Siniflar;
 
 namespace TravelTripProject.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Blogs.OrderByDescending(x => x.ID).Take(8).ToList();
+            return View(degerler);
         }
         public ActionResult About()
         {
-            return View(); 
+            return View();
+        }
+        public PartialViewResult Partial1()
+        {
+            var deger = c.Blogs.OrderByDescending(x=>x.ID).Take(3).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial2()
+        {
+            var deger = c.Blogs.ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial3()
+        {
+            var deger = c.Blogs.Take(3).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial4()
+        {
+            var deger = c.Blogs.Take(3).OrderByDescending(x=>x.ID).ToList();
+            return PartialView(deger);
         }
     }
 }
